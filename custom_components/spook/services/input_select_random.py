bb766ac6bbb0efffb2049680c2e97be83163880f
Franck Nijhof <git@frenck.dev>
2023-02-25 18:43:03 +0100

    Add entity component service: input_select.random

---
+++ custom_components/spook/services/input_select_random.py
@@ -0,0 +1,15 @@
+"""Spook - Not your homey."""
+from __future__ import annotations
+
+from homeassistant.components.input_select import DOMAIN
+
+from .select_random import SpookService as SelectRandomSpookService
+
+
+class SpookService(SelectRandomSpookService):
+    """Input select entity service, select a random option.
+
+    Clone of the select_random service, but for input_select entities.
+    """
+
+    domain = DOMAIN
