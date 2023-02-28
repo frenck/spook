9cf8dbb25f559c6f4c73f505dbe1221ec4cd66b8
Franck Nijhof <git@frenck.dev>
2023-02-28 01:26:24 +0100

    Add service to remove orphaned entities

---
+++ custom_components/spook/services/homeassistant_remove_all_orphaned_entities.py
@@ -0,0 +1,25 @@
+"""Spook - Not your homie."""
+from __future__ import annotations
+
+from homeassistant.components.homeassistant import DOMAIN
+from homeassistant.const import ATTR_RESTORED
+from homeassistant.core import ServiceCall
+from homeassistant.helpers import entity_registry as er
+
+from . import AbstractSpookAdminService
+
+
+class SpookService(AbstractSpookAdminService):
+    """Home Assistant Core integration service to remove all orphaned entities."""
+
+    domain = DOMAIN
+    service = "remove_all_orphaned_entities"
+
+    async def async_handle_service(self, call: ServiceCall) -> None:
+        """Handle the service call."""
+        entity_registry = er.async_get(self.hass)
+        for state in self.hass.states.async_all():
+            if not state.attributes.get(ATTR_RESTORED):
+                continue
+            entity_registry.async_remove(state.entity_id)
+            self.hass.states.async_remove(state.entity_id, call.context)
