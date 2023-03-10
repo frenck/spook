46c5ed814dfb068e6d4f352a2229180fe23635c5
Franck Nijhof <git@frenck.dev>
2023-03-10 22:32:39 +0100

    Add service: Remove entity to area (#55)

---
+++ custom_components/spook/services/homeassistant_remove_entity_from_area.py
@@ -0,0 +1,33 @@
+"""Spook - Not your homie."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import voluptuous as vol
+from homeassistant.components.homeassistant import DOMAIN
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import entity_registry as er
+
+from . import AbstractSpookAdminService
+
+if TYPE_CHECKING:
+    from homeassistant.core import ServiceCall
+
+
+class SpookService(AbstractSpookAdminService):
+    """Home Assistant service to remove an entity from an area."""
+
+    domain = DOMAIN
+    service = "remove_entity_from_area"
+    schema = {
+        vol.Required("entity_id"): vol.All(cv.ensure_list, [cv.string]),
+    }
+
+    async def async_handle_service(self, call: ServiceCall) -> None:
+        """Handle the service call."""
+        entity_registry = er.async_get(self.hass)
+        for entity_id in call.data["entity_id"]:
+            entity_registry.async_update_entity(
+                entity_id,
+                area_id=None,
+            )
