c6e3eda444fb2c412279b70a3837a03c2b4c6eaf
Franck Nijhof <git@frenck.dev>
2023-06-02 10:59:23 +0200

    Fix script has no attribute referenced_devices

--- custom_components/spook/repairs/script_unknown_device_references.py
+++ custom_components/spook/repairs/script_unknown_device_references.py
@@ -30,7 +30,7 @@ class SpookRepair(AbstractSpookRepair):
         for entity in self._entity_component.entities:
             if unknown_devices := {
                 device
-                for device in entity.referenced_devices - devices
+                for device in entity.script.referenced_devices - devices
                 if isinstance(device, str)
             }:
                 self.async_create_issue(
