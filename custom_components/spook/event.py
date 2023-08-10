01c123c50fdb60b1f8d2d18d2b1233bff91f3ee4
Franck Nijhof <git@frenck.dev>
2023-08-10 19:25:36 +0200

    Add support for event entities in ectoplasms (#293)

---
+++ custom_components/spook/event.py
@@ -0,0 +1,27 @@
+"""Spook - Not your homie."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from homeassistant.const import Platform
+
+from .util import async_forward_platform_entry_setups_to_ectoplasm
+
+if TYPE_CHECKING:
+    from homeassistant.config_entries import ConfigEntry
+    from homeassistant.core import HomeAssistant
+    from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up Spook events."""
+    await async_forward_platform_entry_setups_to_ectoplasm(
+        hass,
+        entry,
+        async_add_entities,
+        Platform.EVENT,
+    )
