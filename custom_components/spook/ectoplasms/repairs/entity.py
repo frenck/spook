a304a83dfa2a439860075bd787c2c9f8f505232f
Franck Nijhof <git@frenck.dev>
2023-08-10 21:28:34 +0200

    Add repairs issue sensors

---
+++ custom_components/spook/ectoplasms/repairs/entity.py
@@ -0,0 +1,24 @@
+"""Spook - Not your homie."""
+from __future__ import annotations
+
+from homeassistant.components import repairs
+from homeassistant.const import __version__
+from homeassistant.helpers.entity import DeviceInfo
+
+from ...const import DOMAIN
+from ...entity import SpookEntity, SpookEntityDescription
+
+
+class RepairsSpookEntity(SpookEntity):
+    """Defines an base Spook entity for Repairs related entities."""
+
+    def __init__(self, description: SpookEntityDescription) -> None:
+        """Initialize the entity."""
+        super().__init__(description=description)
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, repairs.DOMAIN)},
+            manufacturer="Home Assistant",
+            name="Repairs",
+            sw_version=__version__,
+        )
+        self._attr_unique_id = f"{repairs.DOMAIN}_{description.key}"
