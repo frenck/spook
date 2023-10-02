4ca031f66f1ab113bd9c452fc00db275621e4835
Maximilian Ertl <Sirs0ri@users.noreply.github.com>
2023-10-03 01:48:15 +0200

    bugfix: remove leftover error log used for debugging
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

--- custom_components/spook/ectoplasms/scene/repairs/unknown_entity_references.py
+++ custom_components/spook/ectoplasms/scene/repairs/unknown_entity_references.py
@@ -37,7 +37,6 @@ class SpookRepair(AbstractSpookRepair):
         ].entities.values()
 
         for entity in scenes:
-            LOGGER.debug("Inspecting scene: %s", entity.entity_id)
             if unknown_entities := {
                 entity_id
                 for entity_id in entity.scene_config.states
