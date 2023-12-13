2e2cae4a61866739f9d609dbbc7045e4da94058d
Bendik R. Brenne <bendik@konstant.no>
2023-12-13 07:41:42 +0100

    Converted to using entity service

--- custom_components/spook/ectoplasms/timer/services/set_duration.py
+++ custom_components/spook/ectoplasms/timer/services/set_duration.py
@@ -5,62 +5,59 @@ from typing import TYPE_CHECKING
 
 import voluptuous as vol
 
+from homeassistant.const import CONF_ID
 from homeassistant.components.timer import (
   CONF_DURATION,
   DOMAIN,
   Timer,
   TimerStorageCollection,
+  _format_timedelta,
 )
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers.entity_component import DATA_INSTANCES
 
-from ....services import AbstractSpookAdminService
+from ....services import AbstractSpookEntityService
 
 if TYPE_CHECKING:
   from homeassistant.core import ServiceCall
-  from homeassistant.helpers.entity_component import EntityComponent
 
 
-class SpookService(AbstractSpookAdminService):
-  """Home Assistant service to add a device tracker to a person."""
+class SpookService(AbstractSpookEntityService):
+  """Home Assistant service to set duration for a timer."""
 
   domain = DOMAIN
+  platform = "timer"
   service = "set_duration"
   schema = {
-    vol.Required("entity_id"): cv.entity_domain(DOMAIN),
     vol.Required(CONF_DURATION): cv.time_period,
   }
 
-  async def async_handle_service(self, call: ServiceCall) -> None:
+
+  async def async_handle_service(
+    self,
+    entity: Timer,
+    call: ServiceCall,
+  ) -> None:
     """Handle the service call."""
-    entity_component: [EntityComponent[Timer]] = self.hass.data[DATA_INSTANCES][
-      DOMAIN
-    ]
+    entity_id = entity.entity_id
 
-    collection: TimerStorageCollection
-    if DOMAIN in self.hass.data:
-      collection = self.hass.data[DOMAIN]
-    else:
-      # Major hack borrowed from ../../zone/services/create.py:27  👻
-      collection = self.hass.data["websocket_api"]["timer/list"][
-        0
-      ].__self__.storage_collection
-
-    if not (entity := entity_component.get_entity(call.data["entity_id"])):
-      message = f"Could not find entity_id: {call.data['entity_id']}"
-      raise HomeAssistantError(message)
-
-    # pylint: disable-next=protected-access
-    if not entity.editable or "id" not in entity._config:  # noqa: SLF001
-      message = f"This timer is not editable: {call.data['entity_id']}"
+    if not entity.editable or not entity.unique_id:
+      message = f"This timer is not editable: {entity_id}"
       raise HomeAssistantError(message)
 
     # pylint: disable-next=protected-access
     updates = entity._config.copy()  # noqa: SLF001
-    item_id = updates.pop("id")
+    item_id = updates.pop(CONF_ID)
     updates.update({
-      CONF_DURATION: call.data[CONF_DURATION],
+      CONF_DURATION: _format_timedelta(call.data[CONF_DURATION]),
     })
+
+    if DOMAIN in entity.hass.data:
+      collection: TimerStorageCollection = entity.hass.data[DOMAIN]
+    else:
+      # Major hack borrowed from ../../zone/services/create.py:27  👻
+      collection: TimerStorageCollection = entity.hass.data["websocket_api"]["timer/list"][
+        0
+      ].__self__.storage_collection
 
     await collection.async_update_item(item_id, updates)
