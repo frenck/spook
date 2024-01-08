90571ba22b6a90598408a9d61c42e1f0a00d3854
Franck Nijhof <git@frenck.dev>
2024-01-08 14:42:12 +0100

    Modernize sensor entity descriptions (#536)

--- custom_components/spook/ectoplasms/homeassistant/button.py
+++ custom_components/spook/ectoplasms/homeassistant/button.py
@@ -27,20 +27,14 @@ if TYPE_CHECKING:
     from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 
-@dataclass
-class HomeAssistantSpookButtonEntityDescriptionMixin:
-    """Mixin values for Home Assistant related buttons."""
-
-    press_fn: Callable[[HomeAssistant], Awaitable[Any]]
-
-
-@dataclass
+@dataclass(frozen=True, kw_only=True)
 class HomeAssistantSpookButtonEntityDescription(
     SpookEntityDescription,
     ButtonEntityDescription,
-    HomeAssistantSpookButtonEntityDescriptionMixin,
 ):
     """Class describing Spook Home Assistant button entities."""
+
+    press_fn: Callable[[HomeAssistant], Awaitable[Any]]
 
 
 BUTTONS: tuple[HomeAssistantSpookButtonEntityDescription, ...] = (
