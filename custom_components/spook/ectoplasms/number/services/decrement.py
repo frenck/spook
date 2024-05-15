c45f5b4d52472986fd6528f6f15eefd15bfc0150
Franck Nijhof <frenck@frenck.nl>
2024-05-15 10:20:17 +0200

    Fix decimal math inaccuracy in increment/decrement services

--- custom_components/spook/ectoplasms/number/services/decrement.py
+++ custom_components/spook/ectoplasms/number/services/decrement.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+import math
 
 import voluptuous as vol
 
@@ -27,7 +28,8 @@ class SpookService(AbstractSpookEntityComponentService[NumberEntity]):
         call: ServiceCall,
     ) -> None:
         """Handle the service call."""
-        if (amount := call.data.get("amount", entity.step or 1)) % entity.step != 0:
+        amount = call.data.get("amount", entity.step or 1)
+        if not math.isclose(amount % entity.step, 0, abs_tol=1e-9):
             msg = (
                 f"Amount {amount} not valid for {entity.entity_id}, "
                 f"it needs to be a multiple of {entity.step}",
