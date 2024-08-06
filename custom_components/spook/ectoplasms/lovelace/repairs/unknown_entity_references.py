308f4a3f3fc03bf9652058d954605eda3ac41b54
Paul Bottein <paul.bottein@gmail.com>
2024-08-06 18:43:57 +0200

    Search for unknown entities inside sections (#798)

--- custom_components/spook/ectoplasms/lovelace/repairs/unknown_entity_references.py
+++ custom_components/spook/ectoplasms/lovelace/repairs/unknown_entity_references.py
@@ -95,12 +95,31 @@ class SpookRepair(AbstractSpookRepair):
         entities = set()
         if isinstance(config, dict) and (views := config.get("views")):
             for view in views:
-                if badges := view.get("badges"):
-                    for badge in badges:
-                        entities.update(self.__async_extract_entities_from_badge(badge))
-                if cards := view.get("cards"):
-                    for card in cards:
-                        entities.update(self.__async_extract_entities_from_card(card))
+                entities.update(self.__async_extract_entities_from_view(view))
+        return entities
+
+    @callback
+    def __async_extract_entities_from_view(self, config: dict[Any]) -> set[str]:
+        """Extract entities from a view config."""
+        entities = set()
+        if badges := config.get("badges"):
+            for badge in badges:
+                entities.update(self.__async_extract_entities_from_badge(badge))
+        if cards := config.get("cards"):
+            for card in cards:
+                entities.update(self.__async_extract_entities_from_card(card))
+        if sections := config.get("sections"):
+            for section in sections:
+                entities.update(self.__async_extract_entities_from_section(section))
+        return entities
+
+    @callback
+    def __async_extract_entities_from_section(self, config: dict[Any]) -> set[str]:
+        """Extract entities from a section config."""
+        entities = set()
+        if cards := config.get("cards"):
+            for card in cards:
+                entities.update(self.__async_extract_entities_from_card(card))
         return entities
 
     @callback
