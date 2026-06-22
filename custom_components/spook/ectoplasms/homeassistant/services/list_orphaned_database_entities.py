76d73356c903ee30f396cfa014947e78285e0c6d
Frenckatron <frenckatron@frenck.nl>
2026-06-22 23:45:48 +0200

    Purge orphaned database entities

--- custom_components/spook/ectoplasms/homeassistant/services/list_orphaned_database_entities.py
+++ custom_components/spook/ectoplasms/homeassistant/services/list_orphaned_database_entities.py
@@ -7,15 +7,42 @@ from typing import TYPE_CHECKING
 from sqlalchemy import create_engine, text
 
 from homeassistant.components.homeassistant import DOMAIN
-from homeassistant.components.recorder import (
-    get_instance,
-)
+from homeassistant.components.recorder import get_instance
 from homeassistant.core import ServiceResponse, SupportsResponse
 
 from ....services import AbstractSpookService
 
 if TYPE_CHECKING:
-    from homeassistant.core import ServiceCall
+    from collections.abc import Iterable
+
+    from homeassistant.core import HomeAssistant, ServiceCall
+
+
+def _list_database_entity_ids(db_url: str) -> list[str]:
+    """List entity IDs stored in the recorder database."""
+    query = text(
+        """
+        SELECT DISTINCT(entity_id) FROM states_meta
+        """
+    )
+    engine = create_engine(db_url)
+    try:
+        with engine.connect() as conn:
+            response = conn.execute(query)
+            return [entity_id for (entity_id,) in response]
+    finally:
+        engine.dispose()
+
+
+async def async_get_orphaned_database_entities(
+    hass: HomeAssistant,
+) -> set[str]:
+    """Return recorder entity IDs that are not in the state machine."""
+    db_url = get_instance(hass).db_url
+    db_list: Iterable[str] = await hass.async_add_executor_job(
+        _list_database_entity_ids, db_url
+    )
+    return set(db_list).difference(hass.states.async_entity_ids())
 
 
 class SpookService(AbstractSpookService):
@@ -27,21 +54,10 @@ class SpookService(AbstractSpookService):
 
     async def async_handle_service(self, call: ServiceCall) -> ServiceResponse:
         """Handle the service call."""
-        query = text(
-            """
-            SELECT DISTINCT(entity_id) FROM states_meta
-            """
-        )
-        db_url = get_instance(self.hass).db_url
-        engine = create_engine(db_url)
-        with engine.connect() as conn:
-            response = conn.execute(query)
-            db_list = [e[0] for e in response]
-        states_list = self.hass.states.async_entity_ids()
-        compared_list = set(db_list).difference(states_list)
+        entities = await async_get_orphaned_database_entities(self.hass)
         if call.return_response:
             return {
-                "count": len(compared_list),
-                "entities": list(compared_list),
+                "count": len(entities),
+                "entities": sorted(entities),
             }
         return None
