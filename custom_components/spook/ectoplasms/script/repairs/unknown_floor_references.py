fade1a72bef353f1727c86f66fcc2470a71ff63c
Franck Nijhof <git@frenck.dev>
2026-07-03 13:14:09 +0000

    Migrate script reference repairs to the shared base class

--- custom_components/spook/ectoplasms/script/repairs/unknown_floor_references.py
+++ custom_components/spook/ectoplasms/script/repairs/unknown_floor_references.py
@@ -2,16 +2,19 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from homeassistant.components import script
 from homeassistant.helpers import floor_registry as fr
-from homeassistant.helpers.entity_component import DATA_INSTANCES, EntityComponent
 
-from ....const import LOGGER
 from ....entity_filtering import async_filter_known_floor_ids, async_get_all_floor_ids
-from ....repairs import AbstractSpookRepair
+from ....repairs import AbstractSpookEntityComponentUnknownReferencesRepair
+
+if TYPE_CHECKING:
+    from typing import Any
 
 
-class SpookRepair(AbstractSpookRepair):
+class SpookRepair(AbstractSpookEntityComponentUnknownReferencesRepair):
     """Spook repair tries to find unknown referenced floors in scripts."""
 
     domain = script.DOMAIN
@@ -19,41 +22,21 @@ class SpookRepair(AbstractSpookRepair):
     inspect_events = {fr.EVENT_FLOOR_REGISTRY_UPDATED}
     inspect_on_reload = True
 
-    automatically_clean_up_issues = True
+    unavailable_entity_class = script.UnavailableScriptEntity
+    entity_label = "script"
+    reference_label = "floors"
+    edit_url_pattern = "/config/script/edit/{unique_id}"
 
-    async def async_inspect(self) -> None:
-        """Trigger a inspection."""
-        if self.domain not in self.hass.data[DATA_INSTANCES]:
-            return
+    _known_floor_ids: set[str]
 
-        entity_component: EntityComponent[script.ScriptEntity] = self.hass.data[
-            DATA_INSTANCES
-        ][self.domain]
+    async def _async_setup_inspection(self) -> None:
+        """Cache known floor IDs for this inspection cycle."""
+        self._known_floor_ids = async_get_all_floor_ids(self.hass)
 
-        known_floor_ids = async_get_all_floor_ids(self.hass)
-
-        LOGGER.debug("Spook is inspecting: %s", self.repair)
-        for entity in entity_component.entities:
-            self.possible_issue_ids.add(entity.entity_id)
-            if not isinstance(entity, script.UnavailableScriptEntity) and (
-                unknown_floors := async_filter_known_floor_ids(
-                    self.hass,
-                    floor_ids=entity.script.referenced_floors,
-                    known_floor_ids=known_floor_ids,
-                )
-            ):
-                self.async_create_issue(
-                    issue_id=entity.entity_id,
-                    translation_placeholders={
-                        "floors": "\n".join(f"- `{floor}`" for floor in unknown_floors),
-                        "script": entity.name,
-                        "edit": f"/config/script/edit/{entity.unique_id}",
-                        "entity_id": entity.entity_id,
-                    },
-                )
-                LOGGER.debug(
-                    "Spook found unknown floors in %s "
-                    "and created an issue for it; Floors: %s",
-                    entity.entity_id,
-                    ", ".join(unknown_floors),
-                )
+    async def _async_compute_unknown_references(self, entity: Any) -> set[str]:
+        """Return unknown floor IDs referenced by ``entity``."""
+        return async_filter_known_floor_ids(
+            self.hass,
+            floor_ids=entity.script.referenced_floors,
+            known_floor_ids=self._known_floor_ids,
+        )
